231d44dd3018c69c331afc7b049511c0d27336e0
Clyde C Brown <clyde.c.brown@gmail.com>
2023-12-15 18:15:55 -0800

    subprocess

---
+++ tests/test_subprocess.py
@@ -0,0 +1,56 @@
+"""Unit tests"""
+import os
+
+from _pytest.logging import LogCaptureFixture
+
+from ccb_essentials.subprocess import subprocess_command, shell_escape
+
+
+class TestSubprocessCommand:
+    """Unit tests for subprocess_command()"""
+
+    @staticmethod
+    def test_list_files() -> None:
+        """It should list files from the filesystem."""
+        expected = ["bin", "etc", "tmp"]
+        results_str = subprocess_command("ls /")
+        assert isinstance(results_str, str)
+        results = results_str.split()
+        for file in expected:
+            assert results.__contains__(file)
+
+    @staticmethod
+    def test_fail() -> None:
+        """It should fail gracefully on an unknown command."""
+        assert subprocess_command("not a command") is None
+
+    @staticmethod
+    def test_report_error(caplog: LogCaptureFixture) -> None:  # type: ignore[misc,no-any-unimported]
+        """It print stderr from a failed command."""
+        assert subprocess_command("not a command") is None
+        assert "not found" in caplog.text or "non-zero exit status" in caplog.text
+
+    @staticmethod
+    def test_strip() -> None:
+        """It should strip whitespace from results."""
+        raw = subprocess_command("uptime", strip_output=False)
+        assert isinstance(raw, str)
+        assert raw.__contains__("load average")
+        assert raw.endswith(os.linesep)
+
+        stripped = subprocess_command("uptime", strip_output=True)
+        assert isinstance(stripped, str)
+        assert stripped.__contains__("load average")
+        assert not stripped.endswith(os.linesep)
+
+    @staticmethod
+    def test_shell_escape() -> None:
+        """It should escape special characters for use in a shell."""
+        for test in [
+            ('', '""'),
+            ('/', '"/"'),
+            ('/foo/test1.txt', '"/foo/test1.txt"'),
+            ('/foo/path with whitespace.txt', '"/foo/path with whitespace.txt"'),
+            ("""abc"def' `hij""", '''"abc\\"def' \\`hij"'''),
+        ]:
+            assert shell_escape(test[0]) == test[1]
